1c97ac54cfaee957a0a7cc394e54cb9f0ce9693b
aluno <aluno@lab01-02>
2026-08-04 14:15:52 -0300

    First Window created

--- gestaoDeViaturas/src/main/java/com/ibirama/gestaodeviaturas/App.java
+++ gestaoDeViaturas/src/main/java/com/ibirama/gestaodeviaturas/App.java
@@ -2,8 +2,10 @@ package com.ibirama.gestaodeviaturas;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -14,13 +16,28 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        //Primeira Tela
+        VBox root = new VBox();
+        
+        ButtonBar buttons = new ButtonBar();
+        Button registerBt = new Button("Registrar");
+        Button loginBt = new Button("Login");
+        buttons.getButtons().addAll(registerBt, loginBt);
+        
+        Label titleHome = new Label("Gestão de Viaturas");
+        
+        root.getChildren().addAll(buttons, titleHome);
+        
+        Scene scene = new Scene(root, 480, 480);
         stage.setScene(scene);
         stage.show();
+    }
+    
+    public void register() {
+        Stage registerWindow = new Stage();
+        VBox root = new VBox();
+        
+        Label title = new Label("Cadastrar");
     }
 
     public static void main(String[] args) {
