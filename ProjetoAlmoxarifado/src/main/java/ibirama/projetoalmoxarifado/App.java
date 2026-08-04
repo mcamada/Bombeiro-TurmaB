3da917fd89cfcf2a363aa929527e2b597768b262
aluno <aluno@lab01-07>
2026-08-04 14:03:18 -0300

    04/08/2026

--- ProjetoAlmoxarifado/src/main/java/ibirama/projetoalmoxarifado/App.java
+++ ProjetoAlmoxarifado/src/main/java/ibirama/projetoalmoxarifado/App.java
@@ -26,7 +26,7 @@ public class App extends Application {
         TextField campo = new TextField();
         PasswordField senha = new PasswordField();
         root.getChildren().addAll(campo, senha);
-        Scene scene = new Scene (root, 640, 480);
+        Scene scene = new Scene (root, 800, 600);
         stage.setScene(scene);
         stage.show();
     }
