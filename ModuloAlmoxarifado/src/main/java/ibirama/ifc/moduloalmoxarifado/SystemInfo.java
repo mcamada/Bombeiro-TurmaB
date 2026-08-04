bd7717448f7243565f0c14076d033c6f577b4298
aluno <aluno@lab01-09>
2026-08-04 13:35:04 -0300

    Segundo Commit

---
+++ ModuloAlmoxarifado/src/main/java/ibirama/ifc/moduloalmoxarifado/SystemInfo.java
@@ -0,0 +1,13 @@
+package ibirama.ifc.moduloalmoxarifado;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
