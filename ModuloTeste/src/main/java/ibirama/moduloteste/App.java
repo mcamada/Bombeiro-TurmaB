a6c3e09cb4cd6f88a6744a4d36f27e28956b2389
aluno <aluno@lab01-03>
2026-08-17 09:18:49 -0300

    mudança 2

--- ModuloTeste/src/main/java/ibirama/moduloteste/App.java
+++ ModuloTeste/src/main/java/ibirama/moduloteste/App.java
@@ -1,8 +1,12 @@
 package ibirama.moduloteste;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,11 +18,23 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        
+        GridPane root = new GridPane();
+        Scene scene = new Scene(root, 800, 600);
+        
+        ButtonBar painelBt = new ButtonBar();
+        Button btCadastrarVt = new Button("CadastrarVt");
+        Button btConsultarVt = new Button("ConsultarVt");
+        Button btExcluirVt = new Button("ExcluirVt");
+        
+        painelBt.getButtons().addAll(btCadastrarVt, btConsultarVt, btExcluirVt);
+    
+        root.add(new Label("Gestão de Viaturas"), 0,1);
+        
+        root.add(btCadastrarVt, 2, 2);
+        root.add(btConsultarVt, 3, 2);
+        root.add(btExcluirVt, 4, 2);
+        
         stage.setScene(scene);
         stage.show();
     }
