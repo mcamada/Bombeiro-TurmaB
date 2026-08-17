5a0500e6217b5dc75ab13a248996612262e821fe
aluno <aluno@lab03-06>
2026-08-17 11:00:47 -0300

    commit

---
+++ ModuloAlmoxarifado/src/main/java/ibirama/ifc/moduloalmoxarifado/Item.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ibirama.ifc.moduloalmoxarifado;
+
+/**
+ *
+ * @author aluno
+ */
+public class Item {
+
+    String nome;
+    String categoria;
+    int quantidade;
+    String unidade;
+    String local;
+    int nivelMinimo;
+    private boolean selecionado;
+
+    public Item(String nome, String categoria, int quantidade,
+            String unidade, String local, int nivelMinimo) {
+
+        this.nome = nome;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
+        this.unidade = unidade;
+        this.local = local;
+        this.nivelMinimo = nivelMinimo;
+        this.selecionado = false;
+    }
+
+    public boolean isSelecionado() {
+        return selecionado;
+    }
+
+    public void setSelecionado(boolean selecionado) {
+        this.selecionado = selecionado;
+    }
+ 
+}
