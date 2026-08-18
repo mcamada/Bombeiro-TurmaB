ff7fd5fe198160d2bc1281f81808355fdfc9eede
aluno <aluno@lab01-21>
2026-08-18 14:15:11 -0300

    Criaçao da tela de ocorrencia

--- SistemadeOcorrencias/src/main/java/com/mycompany/sistemadeocorrencias/TelaOcorrencias.java
+++ SistemadeOcorrencias/src/main/java/com/mycompany/sistemadeocorrencias/TelaOcorrencias.java
@@ -4,8 +4,12 @@
  */
 package com.mycompany.sistemadeocorrencias;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -13,15 +17,133 @@ import javafx.stage.Stage;
  * @author aluno
  */
 public class TelaOcorrencias {
-    public void abrir(Stage stage){
+    
+    public void mostrar(Stage stage) {
         
-   BorderPane root = new BorderPane();
-   
-   root.setStyle("-fx-background-color:# d5b2ad;");
-   
-   Label titulo = new Label("Ocorrencias");
-   titulo.setStyle("-fx-font-size:34px;-fx-text-fill:white;");
-   
-   
+        VBox root = new VBox(20);
+        
+        root.setStyle(
+        "-fx-backGround-color: #C9A5A5;" +
+                "-fx-padding: 35;"
+        );
+        
+        
+        HBox topo = new HBox();
+        
+        Label titulo = new Label("Ocorrencias");
+        
+        titulo.setStyle(
+                "-fx-text-fill: white;" +
+                        "-fx-font-size: 48px;"
+        );
+        
+        Button btNova = new Button("+ Nova Ocorrencia");
+        
+        btNova.setPrefWidth(220);
+        btNova.setPrefHeight(45);
+        
+        btNova.setStyle(
+                "-fx-background-color: #5B171F;" +
+                        "-fx-text-fill: white;" +
+                        "-fx-font size: 16px;" +
+                        "-fx-background-radius: 20;"
+        );
+        topo.setAligment(Pos.CENTER_LEFT);
+        topo.setSpacing(500);
+        
+        topo.getChildren().addAll(
+                titulo,
+                btNova
+        );
+        
+        VBox formulario = new VBox(8);
+        
+        formulario.setPrefWidth(430);
+        
+        formulario.setStyle(
+        "-fx-background-color: 3F4E9DD;" +
+                "-fx-border-color: #5B171F;" +
+                "-fx-border-width: 9;" +
+                "-fx-border-radius: 30;" +
+                "-fx-background-radius: 30;" +
+                "-fx-padding: 25;"
+        );
+        
+        Label tituloForm =
+                new Label("Nova ocorrencia");
+        
+        tituloForm.setStyle(
+                "-fx-text-fill: 35B171F;" +
+                        "-fx-font-size: 27px;" +
+                        "-fx-font-weight: bold;"
+        );
+        
+        Label lblTipo = new Label("Tipo");
+        TextField txtTipo = new TextField();
+             
+        Label lblData = new Label("Data");
+        TextField txtData = new TextField();
+        
+        Label lblHora = new Label("Hora");
+        TextField txtHora = new TextField();
+        
+        Label lblLocal = new Label("Local");
+        TextField txtLocal = new TextField();
+        
+        Label lblDescricao = new Label("Descricao");
+        TextField txtDescricao = new TextField();
+        
+        Label lblStatus = new Label("Status");
+        TextField txtStatus = new TextField();
+        
+        Label lblSinistro = new Label("Sinistro");
+        TextField txtSinistro = new TextField();
+        
+        Label lblPrioridade = new Label("Prioridade");
+        TextField txtPrioridade = new TextField();
+        
+        Button btLimpar = new Button("Limpar");
+        Button btSalvar = new Button("Salvar");
+        
+        btLimpar.setPrefWidth(170);
+        btSalvar.setPrefWidth(170);
+        
+        HBox botoes = new HBox(25);
+        
+        botoes.getChildren().addAll(
+                btLimpar,
+                btSalvar
+        );
+        
+        formulario.getChildren().addAll(
+                tituloForm,
+                
+                lblTipo,
+                txtTipo,
+                
+                lblData,
+                txtData,
+                
+                lblHora,
+                txtHora,
+                
+                lblLocal,
+                txtlocal,
+                
+                lblDescricao,
+                txtDescricao,
+                
+                lblStatus,
+                txtStatus,
+                
+                lblSinistro,
+                txtSinitro,
+                
+                lblPrioridade,
+                txtPrioridade,
+                
+                botoes
+        );
+                
     }
 }
