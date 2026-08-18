620cb2dae983471af2efb593126cc3a54d4d0c91
aluno <aluno@lab01-10>
2026-08-18 14:12:29 -0300

    Mudança 3

--- ModuloTeste/src/main/java/ibirama/moduloteste/App.java
+++ ModuloTeste/src/main/java/ibirama/moduloteste/App.java
@@ -1,15 +1,14 @@
 package ibirama.moduloteste;
 
-
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 /**
  * JavaFX App
@@ -18,23 +17,42 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        
+
         GridPane root = new GridPane();
         Scene scene = new Scene(root, 800, 600);
-        
-        ButtonBar painelBt = new ButtonBar();
-        Button btCadastrarVt = new Button("CadastrarVt");
-        Button btConsultarVt = new Button("ConsultarVt");
-        Button btExcluirVt = new Button("ExcluirVt");
-        
-        painelBt.getButtons().addAll(btCadastrarVt, btConsultarVt, btExcluirVt);
-    
-        root.add(new Label("Gestão de Viaturas"), 0,1);
-        
-        root.add(btCadastrarVt, 2, 2);
-        root.add(btConsultarVt, 3, 2);
-        root.add(btExcluirVt, 4, 2);
-        
+
+        Button btCadastrarVt = new Button("Cadastrar Vt");
+        Button btConsultarVt = new Button("Consultar Vt");
+        Button btExcluirVt = new Button("Excluir Vt");
+        Button btCadastrarUs = new Button("Cadastrar Usuário");
+
+        Label titulo = new Label("Gestão de Viaturas");
+
+        // Gestão de Viaturas
+        // Sozinho no topo e centralizado
+        root.add(titulo, 1, 0);
+        GridPane.setHalignment(titulo, HPos.CENTER);
+
+        // Cadastrar Usuário
+        // Linha própria, alinhado com Excluir Vt
+        root.add(btCadastrarUs, 2, 1);
+        GridPane.setHalignment(btCadastrarUs, HPos.CENTER);
+
+        // Botões de Viaturas
+        root.add(btCadastrarVt, 0, 2);
+        root.add(btConsultarVt, 1, 2);
+        root.add(btExcluirVt, 2, 2);
+
+        // Espaçamento entre os elementos
+        root.setHgap(30);
+        root.setVgap(20);
+
+        // GridPane no topo e centralizado
+        root.setAlignment(Pos.TOP_CENTER);
+
+        // Distância do topo da janela
+        root.setPadding(new Insets(25, 20, 20, 20));
+
         stage.setScene(scene);
         stage.show();
     }
@@ -42,5 +60,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
