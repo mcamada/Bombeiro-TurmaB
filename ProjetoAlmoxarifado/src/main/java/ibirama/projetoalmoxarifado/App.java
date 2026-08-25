cdfa6da7d4afcc384d85eb80b530940d089917e2
aluno <aluno@lab01-06>
2026-08-25 14:14:22 -0300

    25/08/26

--- ProjetoAlmoxarifado/src/main/java/ibirama/projetoalmoxarifado/App.java
+++ ProjetoAlmoxarifado/src/main/java/ibirama/projetoalmoxarifado/App.java
@@ -1,7 +1,9 @@
 package ibirama.projetoalmoxarifado;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -24,20 +26,27 @@ public class App extends Application {
     public void start(Stage stage) {
         VBox root = new VBox();
 
-        Label Texto1 = new Label("Usuário");
-        Label Texto2 = new Label("Senha");
+        Label usuario = new Label("Usuário");
+        Label senha = new Label("Senha");
 
-        TextField campo = new TextField();
-        PasswordField senha = new PasswordField();
+        TextField campoUsuario = new TextField();
+        PasswordField campoSenha = new PasswordField();
 
         Button btCancelar = new Button("Cancelar");
         Button btConfirmar = new Button("Confirmar");
 
         ButtonBar painelBt = new ButtonBar();
         painelBt.getButtons().addAll(btCancelar, btConfirmar);
-        root.getChildren().addAll(Texto1, campo, Texto2, senha, painelBt);
+        root.getChildren().addAll(usuario, campoUsuario, senha, campoSenha, painelBt);
+        
+        Scene cena = new Scene (root, 800, 600);
+        stage.setTitle ("login");
+        stage.setScene(cena);
+        stage.show();
+        
+       
 
-        EventHandler<MouseEvent> eh = new EventHandler<MouseEvent>() {
+        EventHandler<MouseEvent> cadastro = new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -48,7 +57,13 @@ public class App extends Application {
                 Stage cadastro = new Stage();
                 GridPane rootMenu = new GridPane();
 
-                btConfirmar.addEventHandler(MouseEvent.MOUSE_CLICKED, eh);
+                rootMenu.add(new Label("Consulta"), 0, 0);
+                rootMenu.add(new Label("Atualização"), 0, 2);
+                rootMenu.add(new Label("Exclusão"), 2, 0);
+                rootMenu.add(new Label("Cadastro"), 2, 2);
+
+                EventHandler<ActionEvent> confirmarLogin;
+
                 Scene cenadrato = new Scene(rootMenu, 800, 600);
                 cadastro.setScene(cenadrato);
                 cadastro.show();
@@ -67,10 +82,17 @@ public class App extends Application {
                 Stage consulta = new Stage();
                 VBox rootConsulta = new VBox();
 
-                rootConsulta.getChildren().add(new Label("Consulta de item"));
-                Label quantDisponivel = new Label("Quantidade disponível");
-                TextField quantD = new TextField();
-
+                Label item = new Label("Item");
+                TextField campoItem = new TextField();
+                Label categoria = new Label("Categoria");
+                TextField campoCategoria = new TextField();
+                Label quantDisponivel = new Label("Quantidade disponivel");
+                TextField campoQuantidadeDisponivel = new TextField();
+                Label localizacao = new Label("Localização");
+                TextField campoLocalizacao = new TextField();
+                Button btConsulta = new Button();
+                
+                rootConsulta.getChildren().addAll(item, campoItem, categoria, campoCategoria, quantDisponivel, campoQuantidadeDisponivel, localizacao, campoLocalizacao, btConsulta);
                 Scene cenarioConsulta = new Scene(rootConsulta, 800, 600);
                 consulta.setScene(cenarioConsulta);
                 consulta.show();
@@ -86,11 +108,16 @@ public class App extends Application {
                         Stage atulizacao = new Stage();
                         VBox rootAtualizacao = new VBox();
 
-                        Label quantidadeAjusteOuReposicao = new Label("Quantidade para reposição ou ajuste");
+                        Label quantidadeAR = new Label("Quantidade(ajuste ou reposição)");
+                        TextField campoQuantidadeAR = new TextField();
+                        Label localizacao = new Label("Localização");
+                        TextField campoLocalizacao = new TextField();
+                        Label nivelMinimo = new Label("Nivel minimo");
+                        TextField campoNivelMinimo = new TextField();
                         Label descricao = new Label("Descrição");
-                        TextField descrição = new TextField();
-                        TextField quantAR = new TextField();
-                        rootAtualizacao.getChildren().addAll(quantidadeAjusteOuReposicao, quantAR, localizacaoEstoque, LocalizacaoE, nivelMinimoEstoque, NivelME, descricao, descrição, painelBt);
+                        TextField campoDescricao = new TextField();
+
+                        Button btAtualizar = new Button("Atualizar");
                     }
                 };
 
@@ -106,8 +133,11 @@ public class App extends Application {
                         Label excluirItem = new Label(("Qual item deseja excluir"));
                         TextField exclusaoItem = new TextField();
 
+                        Button btCancelarExclusao = new Button("Cancelar");
+                        Button btConfirmarExclusao = new Button("Confirmar");
+
                         event.consume();
-                        rootExclusao.getChildren().addAll(excluirItem, exclusaoItem, btCancelar, btConfirmar);
+                        rootExclusao.getChildren().addAll(excluirItem, exclusaoItem, btCancelarExclusao, btConfirmarExclusao);
 
                     }
                 };
